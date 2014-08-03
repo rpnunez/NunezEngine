4e5f1e28b99315fdb37fea25b01ea6803b1fddfa
Raymond Nunez <rpnunez@gmail.com>
2014-08-03 00:51:08 -0400

    Included init.php and initialized NunezEngine

--- index.php
+++ index.php
@@ -14,11 +14,22 @@ define('APP_PATH', BASE_PATH . DS . 'app');
 define('SYSTEM_PATH', BASE_PATH . DS . 'system');
 
 try {
+    // Bootstrap
+    $initPath = SYSTEM_PATH . DS . 'init.php';
+    if (!file_exists($initPath)) {
+        throw new \Exception('Fatal Error: SYSTEM_PATH/init.php is missing.');
+    } else {
+        require_once SYSTEM_PATH . DS . 'init.php';
+    }
+
     // Include NunezEngine
     $nePath = SYSTEM_PATH . DS . 'NunezEngine.php';
 
     if (!file_exists($nePath)) {
         throw new \Exception('Fatal Error: SYSTEM_PATH/NunezEngine.php is missing.');
+    } else {
+        require_once $nePath;
+        $NunezEngine = new NunezEngine();
     }
 } catch (\Exception $e) {
     echo 'Caught exception: '. $e->getMessage() .' on file '. $e->getFile() .', line '. $e->getLine() .'.';
