c10be175d5b0dd706ea9c5ea0852fd7cac606f16
Raymond Nunez <rpnunez@gmail.com>
2014-08-03 00:29:18 -0400

    Added path constants and skeleton

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * NunezEngine
+ * Micro-framework for rapid web application development.
+ *
+ * @author Raymond Nunez <rpnunez@gmail.com>
+ * @version v0.1
+ */
+
+define('DS', DIRECTORY_SEPARATOR);
+define('BASE_PATH', dirName(__FILE__));
+define('APP_PATH', BASE_PATH . DS . 'app');
+define('SYSTEM_PATH', BASE_PATH . DS . 'system');
+
+try {
+    // Include NunezEngine
+    $nePath = SYSTEM_PATH . DS . 'NunezEngine.php';
+
+    if (!file_exists($nePath)) {
+        throw new \Exception('Fatal Error: SYSTEM_PATH/NunezEngine.php is missing.');
+    }
+} catch (\Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() .' on file '. $e->getFile() .', line '. $e->getLine() .'.';
+}
