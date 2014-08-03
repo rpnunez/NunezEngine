fca78bbbfb6917387e4f358a277f80be27daaa60
Raymond Nunez <rpnunez@gmail.com>
2014-08-03 00:48:24 -0400

    Initial commit

---
+++ system/init.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * NunezEngine
+ * Micro-framework for rapid web application development.
+ *
+ * @author Raymond Nunez <rpnunez@gmail.com>
+ * @version v0.1
+ */
+
+spl_autoload_register(function() {
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    echo 'Attempting to require_once: '. $className .' ('. $fileName .').<br />';
+    require_once $fileName;
+})
