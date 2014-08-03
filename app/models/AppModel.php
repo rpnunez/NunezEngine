0388a186e7b0d3ca65ba2c44cec838712824aab6
Raymond Nunez <rpnunez@gmail.com>
2014-08-03 00:37:06 -0400

    Initial commit

---
+++ app/models/AppModel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * NunezEngine
+ * Micro-framework for rapid web application development.
+ *
+ * @author Raymond Nunez <rpnunez@gmail.com>
+ * @version v0.1
+ */
+
+namespace NunezEngine;
+
+class AppModel extends EngineModel {
+
+} 
