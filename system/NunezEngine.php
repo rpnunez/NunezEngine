e4d9882511105fcfeec7c31142e2c620d61eebc7
Raymond Nunez <rpnunez@gmail.com>
2014-08-03 00:52:00 -0400

    Added a sexy class loader for core objects

--- system/NunezEngine.php
+++ system/NunezEngine.php
@@ -16,4 +16,30 @@
  */
 class NunezEngine {
 
+    /**
+     * Accessible objects
+     */
+    public $Dispatcher = null;
+    public $Router = null;
+    public $Input = null;
+    public $DB = null;
+
+    /**
+     * Private variables
+     */
+    private $config = null;
+
+    public function __construct() {
+        // Include config
+        $files = array('Config', 'Dispatcher', 'Router', 'Input', 'DB');
+
+        foreach ($files as $file) {
+            if (!file_exists($file .'.php')) {
+                throw new \Exception('Fatal error: Required file '. $file .'.php missing.');
+            } else {
+                require_once($file .'.php');
+                $this->$file = new $file();
+            }
+        }
+    }
 } 
