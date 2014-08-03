ab9bb9f00f567a4db554d8ab00c13a8566367bd4
Raymond Nunez <rpnunez@gmail.com>
2014-08-03 00:54:26 -0400

    Initial commit

---
+++ system/Config.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * NunezEngine
+ * Micro-framework for rapid web application development.
+ *
+ * @author Raymond Nunez <rpnunez@gmail.com>
+ * @version v0.1
+ */
+namespace NunezEngine;
+
+class Config {
+    /**
+     * Turns debug on or off globally.
+     * @var bool
+     */
+    public $debug = true;
+
+    /**
+     * Higher the number, the more debug messages you will see throughout your app.
+     * @var int
+     */
+    public $debugLevel = 10;
+} 
