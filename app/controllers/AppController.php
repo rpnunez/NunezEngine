29f53011c50fb348076a662f7e262554e3c211e3
Raymond Nunez <rpnunez@gmail.com>
2014-08-03 00:36:09 -0400

    Initial commit

---
+++ app/controllers/AppController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * NunezEngine
+ * Micro-framework for rapid web application development.
+ *
+ * @author Raymond Nunez <rpnunez@gmail.com>
+ * @version v0.1
+ */
+
+namespace NunezEngine;
+
+
+class AppController extends EngineController {
+
+} 
